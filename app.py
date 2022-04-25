89e1f067f26b0b13a9d0734eb286e978be83f563
colaTW <53038073+colaTW@users.noreply.github.com>
2022-04-25 17:17:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask import Flask, abort, request
 # https://github.com/line/line-bot-sdk-python
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.models import MessageEvent, TextMessage, TextSendMessage,MemberJoinedEvent
 
 app = Flask(__name__)
 
@@ -30,7 +30,11 @@ def callback():
 
         return "OK"
 
-
+@handler.add(MemberJoinedEvent)
+def handle_memberJoined(event):
+    # Send To Line
+    reply = TextSendMessage(text=f"welcome")
+    line_bot_api.reply_message(event.reply_token, reply)
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     get_message = event.message.text
